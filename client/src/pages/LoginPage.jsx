cc58d737539da3fd6f8915bd28ab353050f05581
Anselme Dor <anselmedor@hotmail.be>
2023-03-02 11:16:30 +0100

    Updating form

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import LoginForm from "../components/LoginForm";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex sm:w-1/2 mx-auto">
+      <LoginForm />
+    </div>
+  );
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ErrorField from "../components/ErrorField";
+import RegisterForm from "../components/RegisterForm";
 
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const [errorField, setErrorField] = useState("");
+  const [showErrorField, setShowErrorField] = useState(false);
+  useEffect(() => {
+    if (errorField?.length > 0) {
+      setShowErrorField(true);
+    }
+  }, [errorField]);
+  return (
+    <>
+      <ErrorField
+        showErrorField={showErrorField}
+        setShowErrorField={setShowErrorField}
+        errorField={errorField}
+        setErrorField={setErrorField}
+      />
+      <div className="flex justify-center sm:w-1/2 mx-auto">
+        <RegisterForm setErrorField={setErrorField} />
+      </div>
+    </>
+  );
 }
