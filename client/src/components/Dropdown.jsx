b02805e6deee0c2c04af2b34154db9d6023ea316
Anselme Dor <anselmedor@hotmail.be>
2023-03-02 11:15:17 +0100

    Adding dropdown

---
+++ client/src/components/Dropdown.jsx
@@ -0,0 +1,114 @@
+import { useState, createContext, useContext, Fragment } from "react";
+import { Transition } from "@headlessui/react";
+import { Link } from "react-router-dom";
+
+export const DropDownContext = createContext();
+
+const Dropdown = ({ children }) => {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen((previousState) => !previousState);
+  };
+
+  return (
+    <DropDownContext.Provider value={{ open, setOpen, toggleOpen }}>
+      <div className="relative">{children}</div>
+    </DropDownContext.Provider>
+  );
+};
+
+const Trigger = ({ children }) => {
+  const { open, setOpen, toggleOpen } = useContext(DropDownContext);
+
+  return (
+    <>
+      <div onClick={toggleOpen}>{children}</div>
+
+      {open && (
+        <div
+          className="fixed inset-0 z-40 h-screen"
+          onClick={() => setOpen(false)}
+        ></div>
+      )}
+    </>
+  );
+};
+
+const Content = ({
+  align = "right",
+  width = "48",
+  contentClasses = "bg-white",
+  children,
+}) => {
+  const { open, setOpen } = useContext(DropDownContext);
+
+  let alignmentClasses = "origin-top";
+
+  if (align === "left") {
+    alignmentClasses = "origin-top-left left-0";
+  } else if (align === "right") {
+    alignmentClasses = "origin-top-right right-0";
+  }
+
+  let widthClasses = "";
+
+  if (width === "48") {
+    widthClasses = "w-48";
+  }
+
+  return (
+    <>
+      <Transition
+        as={Fragment}
+        show={open}
+        enter="transition ease-out duration-300"
+        enterFrom="transform opacity-0 scale-50"
+        enterTo="transform  opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-50"
+      >
+        <div
+          className={`absolute z-50 shadow-lg rounded-lg ${alignmentClasses} ${widthClasses}`}
+        >
+          <div
+            className={"rounded-lg " + contentClasses}
+            onClick={() => setOpen(false)}
+          >
+            {children}
+          </div>
+        </div>
+      </Transition>
+    </>
+  );
+};
+
+const DropdownLink = ({ children, to, className }) => {
+  return (
+    <Link
+      to={to}
+      className={`block w-full px-4 py-2 text-left text-sm text-white bg-primary transition duration-300 hover:bg-[#758761] focus:bg-primary ${className}`}
+    >
+      {children}
+    </Link>
+  );
+};
+
+const DropdownButton = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="block w-full px-4 py-2 text-left text-lg md:text-sm leading-5 text-gray-700 hover:bg-primary hover:text-white focus:bg-primary transition duration-150 ease-in-out"
+    >
+      {children}
+    </button>
+  );
+};
+
+Dropdown.Trigger = Trigger;
+Dropdown.Content = Content;
+Dropdown.Button = DropdownButton;
+Dropdown.Link = DropdownLink;
+
+export default Dropdown;
--- client/src/components/navigation/NavUser.jsx
+++ client/src/components/navigation/NavUser.jsx
@@ -1,23 +1,26 @@
 import React, { useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
+import Dropdown from "../Dropdown";
 
 export default function NavUser() {
-  const [showUserDropdown, setShowUserDropdown] = useState(false);
-
-  const openUserDropdown = () => {
-    setShowUserDropdown(true);
-  };
-
-  const closeUserDropdown = () => {
-    setShowUserDropdown(false);
-  };
-
   return (
-    <button type="button" onClick={openUserDropdown}>
-      <AiOutlineUser
-        size={32}
-        className="transition duration-300 hover:fill-secondary"
-      />
-    </button>
+    <>
+      <Dropdown>
+        <Dropdown.Trigger>
+          <AiOutlineUser
+            size={32}
+            className="cursor-pointer transition duration-300 hover:fill-primary"
+          />
+        </Dropdown.Trigger>
+        <Dropdown.Content contentClasses="font-Jost font-medium uppercase text-2xl tracking-wider">
+          <Dropdown.Link to={"/login"} className={"rounded-t-lg"}>
+            Login
+          </Dropdown.Link>
+          <Dropdown.Link to={"/register"} className={"rounded-b-lg"}>
+            Register
+          </Dropdown.Link>
+        </Dropdown.Content>
+      </Dropdown>
+    </>
   );
 }
