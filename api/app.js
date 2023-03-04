29dbfab6c9e3f7448729c87f39292d8580af6ab2
Anselme Dor <anselmedor@hotmail.be>
2023-03-04 01:26:46 +0100

    validation and error handling login/register form

--- api/app.js
+++ api/app.js
@@ -1,13 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { connect } from "./config/database.js";
 import authRoutes from "./routes/authRoutes.js";
-import { checkJWT as auth } from "./middlewares/auth.js";
+import morgan from "morgan";
 
 dotenv.config();
 connect();
 const app = express();
 
 app.use(express.json());
+app.use(
+  morgan(
+    ":date[clf] - :method :url  -STATUS: :status  -RESPONSE-TIME: :response-time ms"
+  )
+);
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+  })
+);
+
+app.use("/", authRoutes);
 
 export default app;
--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,33 +1,68 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import validator from "validator";
 import UserModel from "../models/user.js";
 
+const bcryptSalt = await bcrypt.genSalt(10);
+
 export async function createUser(req, res) {
+  const { username, email, password, passwordConfirm } = req.body;
+  // validate user input
+  if (!(email && password && username && passwordConfirm)) {
+    return res.status(400).send("All fields are required.");
+  }
+
+  if (!validator.isAlphanumeric(username)) {
+    return res
+      .status(400)
+      .send("Username must only contain alphanumeric characters.");
+  }
+
+  if (
+    !validator.isLength(username, {
+      min: 4,
+      max: 16,
+    })
+  ) {
+    return res
+      .status(400)
+      .send("Username must contain between 4 and 16 characters.");
+  }
+
+  if (!validator.isEmail(email)) {
+    return res.status(400).send("Email is not valid.");
+  }
+
+  const oldUser = await UserModel.findOne({ email });
+  if (oldUser) {
+    return res.status(409).send("Email is already used.");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    return res
+      .status(400)
+      .send(
+        "Password must contain at least 8 character including at least a lowercase and a uppercase letter, a number and a symbol"
+      );
+  }
+
+  if (!validator.equals(password, passwordConfirm)) {
+    return res.status(400).send("Passwords must match.");
+  }
+
   try {
-    const { username, email, password } = req.body;
-    // validate user input
-    if (!(email && password && username)) {
-      res.status(400).send("All input is required");
-    }
-
-    // check if user already exist
-    const oldUser = await UserModel.findOne({ email });
-    if (oldUser) {
-      return res.status(409).send("User Already Exist. Please Login");
-    }
-
-    let encryptedPassword = await bcrypt.hash(password, 10);
+    let encryptedPassword = await bcrypt.hash(password, bcryptSalt);
 
     // create user in our database
     const user = await UserModel.create({
       username,
-      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      email: email.toLowerCase(),
       password: encryptedPassword,
     });
-
     res.status(201).json(user);
   } catch (err) {
-    console.log(err);
+    console.error(err);
+    res.status(400).send(err);
   }
 }
 
@@ -36,15 +71,23 @@ export async function loginUser(req, res) {
     const { email, password } = req.body;
     // validate user input
     if (!(email && password)) {
-      res.status(400).send("All input is required");
+      res.status(400).send("All fields are required");
     }
+    if (!validator.isEmail(email)) {
+      return res.status(400).send("Email is not valid.");
+    }
+
     // validate if user exist in our database
     const user = await UserModel.findOne({ email });
 
-    if (user && (await bcrypt.compare(password, user.password))) {
+    if (!user) {
+      return res.status(400).send("Your are not register.");
+    }
+
+    if (user && (await bcrypt.compareSync(password, user.password))) {
       res.status(200).json(user);
     } else res.status(400).send("Invalid Credentials");
   } catch (err) {
-    console.log(err);
+    res.status(400).json(err);
   }
 }
