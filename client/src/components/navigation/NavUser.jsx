598f9f4a70359fd9bed947c0ca40860113b1ad8b
Anselme Dor <anselmedor@hotmail.be>
2023-03-09 20:53:21 +0100

    Add user cintext to nav

--- client/src/components/navigation/NavUser.jsx
+++ client/src/components/navigation/NavUser.jsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useContext, useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
+import { UserContext } from "../../context/UserContext";
 import Dropdown from "../Dropdown";
 
-export default function NavUser() {
+export default function NavUser({ user }) {
+  const { setUser } = useContext(UserContext);
+
+  const logout = (e) => {
+    e.preventDefault();
+    axios.get("/logout");
+    setTimeout(() => {
+      setUser(null);
+    }, 100);
+  };
   return (
     <>
       <Dropdown>
@@ -12,13 +23,26 @@ export default function NavUser() {
             className="cursor-pointer transition duration-300 hover:fill-primary"
           />
         </Dropdown.Trigger>
-        <Dropdown.Content contentClasses="font-medium uppercase text-2xl tracking-wider">
-          <Dropdown.Link to={"/login"} className={"rounded-t-lg"}>
-            Login
-          </Dropdown.Link>
-          <Dropdown.Link to={"/register"} className={"rounded-b-lg"}>
-            Register
-          </Dropdown.Link>
+        <Dropdown.Content contentClasses="">
+          {user ? (
+            <>
+              <Dropdown.Link to={"/profile"} className={"rounded-t-lg"}>
+                Profile
+              </Dropdown.Link>
+              <Dropdown.Button onClick={logout} className={"rounded-b-lg"}>
+                Logout
+              </Dropdown.Button>
+            </>
+          ) : (
+            <>
+              <Dropdown.Link to={"/login"} className={"rounded-t-lg"}>
+                Login
+              </Dropdown.Link>
+              <Dropdown.Link to={"/register"} className={"rounded-b-lg "}>
+                Register
+              </Dropdown.Link>
+            </>
+          )}
         </Dropdown.Content>
       </Dropdown>
     </>
--- client/src/layouts/Navigation.jsx
+++ client/src/layouts/Navigation.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AiOutlineShoppingCart, AiOutlineUser } from "react-icons/ai";
 import NavListDesktop from "../components/navigation/NavListDesktop";
 import NavLogo from "../components/navigation/NavLogo";
 import NavMobile from "../components/navigation/NavMobile";
 import NavUser from "../components/navigation/NavUser";
 import NavCart from "../components/navigation/NavCart";
+import { UserContext } from "../context/UserContext";
 
 export default function Navigation({ visible }) {
+  const { user, setUser, ready } = useContext(UserContext);
   return (
     <nav className="grid grid-cols-3 items-center py-3 mx-2 sm:mx-8 lg:mx-24 ">
       <NavListDesktop />
       <NavMobile />
       <NavLogo />
       <div className="flex justify-end gap-8">
-        <NavUser />
+        <NavUser user={user} />
         <NavCart visible={visible} />
       </div>
     </nav>
